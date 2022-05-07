053d3202d9e103429ca7d036bcd7f5a515e5e489
johnworker <besscomdark@gmail.com>
2022-05-07 22:10:55 +0800

    自動上升

---
+++ Assets/Scenes/Floor.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Floor : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, moveSpeed*Time.deltaTime, 0);
+    }
+}
