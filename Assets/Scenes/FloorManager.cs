56ab93420f1bfe606031197a3fe15d27e542a8fb
johnworker <besscomdark@gmail.com>
2022-05-07 22:26:22 +0800

    修正程式檔

---
+++ Assets/Scenes/FloorManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorManager : MonoBehaviour
+{
+    [SerializeField] GameObject[] floorPrefabs;
+    public void SpawnFloor()
+    {
+        int r = Random.Range(0, floorPrefabs.Length);
+    }
+}
